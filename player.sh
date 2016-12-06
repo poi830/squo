92400449121e35997e956c0cdc300adec76e4170
Dhilan Lahoti <poi830@gmail.com>
2016-12-06 16:54:46 -0600

    Make player automatic

--- player.sh
+++ player.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-total="`awk -f /home/lahoti/Documents/scripts/bash/player/sum.awk $1`"
-rand=$RANDOM
-#volume=1
-#while getopts v: OPTIONS
-#do
-#	case $opt in
-#		v)
-#			volume=$OPTARG;;
-#		?)
-#			printf "Invalid option: $OPTARG\nUsage: player.sh [-v volume] playlist"
-#			exit 2;;
-#	esac
-#done
-#volume=1
-awk -v total=$total -v RANDOM=$rand -f /home/lahoti/Documents/scripts/bash/player/main.awk $1
+while getopts h OPTIONS
+do
+	case $opt in
+		h)
+			printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: player.sh playlist"
+			exit 0;;
+		?)
+			printf "Invalid option: $OPTARG\nUsage: player.sh playlist"
+			exit 2;;
+	esac
+done
+while :;
+do
+	total="`awk -f /home/lahoti/Documents/scripts/bash/player/sum.awk $1`"
+	rand=$RANDOM
+	mpv `awk -v total=$total -v RANDOM=$rand -f /home/lahoti/Documents/scripts/bash/player/main.awk $1`
+done
