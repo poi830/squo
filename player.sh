7cc69356bc42e11d7c1b883b30ed7ae78610acc6
Dhilan Lahoti <poi830@gmail.com>
2016-12-07 17:48:15 -0600

    Add README.md

--- player.sh
+++ player.sh
@@ -4,10 +4,10 @@ while getopts :h opt
 do
 	case $opt in
 		h)
-			printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: player.sh playlist\n"
+			printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: squo playlist\n"
 			exit 0;;
 		?)
-			printf "Invalid option: $OPTARG\nUsage: player.sh playlist\n"
+			printf "Invalid option: $OPTARG\nUsage: squo playlist\n"
 			exit 2;;
 	esac
 done
