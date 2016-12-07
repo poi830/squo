96cbd61032191de054f5da6932776ceffff18737
Dhilan Lahoti <poi830@gmail.com>
2016-12-07 17:40:18 -0600

    Fix option parsing

--- player.sh
+++ player.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-while getopts h OPTIONS
+while getopts :h opt
 do
 	case $opt in
 		h)
-			printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: player.sh playlist"
+			printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: player.sh playlist\n"
 			exit 0;;
 		?)
-			printf "Invalid option: $OPTARG\nUsage: player.sh playlist"
+			printf "Invalid option: $OPTARG\nUsage: player.sh playlist\n"
 			exit 2;;
 	esac
 done
