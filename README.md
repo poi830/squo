9496717f5d16ff535242de363295b06dfbe89a35
Dhilan Lahoti <poi830@gmail.com>
2016-12-08 17:16:45 -0600

    add config file, fix formatting

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## usage
 
-The script is called as `squo playlist`.
+The script is called as `squo [-vVh] playlist`.
 
 ## playlists
 
 Playlists are specified in the format "/path/to/song volume weight", with a song on each line. The weight specifies how often the song should be played. The default volume is 100, with higher values representing louder volumes. The path to the song cannot contain spaces due to limitations in my ability with awk.
+
+## options
+
+`-v`: sets the `--no-audio-video` option in mpv
+`-V`: unsets the `--no-audio-video` option
+`-h`: prints a help message
+
+## config
+
+The config file is a list of options, one per line.
--- player.sh
+++ player.sh
@@ -2,25 +2,40 @@
 
 audiodisplay=""
 playlist="playlist.txt"
-for i in "$@"
-do
-	if [[ $i = "-h" ]]
-	then
+config="$HOME/.config/squo/config"
+
+if [ ! -e $config ]; then
+	mkdir -p "$HOME/.config/squo"
+	echo "-v" > $config
+fi
+
+while read -r line || [[ -n "$line" ]]; do
+	if [[ $line = "-h" ]]; then
 		printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: player.sh playlist\n"
 		exit 0
-	elif [[ $i = "-v" ]]
-	then
+	elif [[ $line = "-v" ]]; then
 		audiodisplay="--no-audio-display"
-	elif [[ $i = "-V" ]]
-	then
+	elif [[ $line = "-V" ]]; then
+		audiodisplay=""
+	fi
+done < $config
+
+for i in "$@"; do
+	if [[ $i = "-h" ]]; then
+		printf "Simple music player allowing you to change how often a song plays and at what volume.\nUsage: player.sh playlist\n"
+		exit 0
+	elif [[ $i = "-v" ]]; then
+		audiodisplay="--no-audio-display"
+	elif [[ $i = "-V" ]]; then
 		audiodisplay=""
 	else
 		playlist=$i
 	fi
 done
+
 directory=$(dirname -- "$(readlink -f -- "$0")")
-while :;
-do
+
+while true; do
 	total="`awk -f $directory/sum.awk $playlist`"
 	rand=$RANDOM
 	mpv $audiodisplay `awk -v total=$total -v RANDOM=$rand -f $directory/main.awk $playlist`
